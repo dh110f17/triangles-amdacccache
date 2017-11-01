bef72b3a785223435c3f32bfaaa4a6e76a38fb7e
Anna Daccache <annadaccache@annas-mbp-3.ad.wlu.edu>
2017-10-31 23:55:01 -0400

    edit

--- script.js
+++ script.js
@@ -38,7 +38,9 @@ var triangle = {
 var message1 = "The area of a triangle with a base of " + base + " and a height of " + height + " is " + area + ".";
 var message2 = "The hypotenuse of a triangle with a base of " + base + " and a height of " + height + " is " + hypotenuse + ".";
 
-document.getElementById("three").innerHTML = message1 + message2;
+document.getElementById("three").innerHTML = message1 
+//linebreak
+ message2;
 triangle.drawIt();
 
 

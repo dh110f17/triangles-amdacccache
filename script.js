15e7869e29283c1ee3bb0e0a212f1ec7297b5764
Anna Daccache <annadaccache@annas-mbp-3.ad.wlu.edu>
2017-10-31 23:53:54 -0400

    javascript

--- script.js
+++ script.js
@@ -2,14 +2,49 @@
 var base = 30;
 var height = 40;
 var area = 1 / 2 * (base * height);
+var message = "The area of a triangle with a base of " + base + " and a height of " + height + " is " + area + ".";
 
-var Area = {
-    base: 30,
-    height: 40,
-    area: function()
-        return (1 / 2 * (base * height));
-    }
+document.getElementById("one").innerHTML = message;
+
+// Part 2: Hypotenuse of a triangle
+
+function calcHypotenuse (b,h) {
+  var hypotenuse = Math.sqrt((b*b) + (h * h));
+  return hypotenuse
 };
 
-console.log("The area of a triangle with a base of " + Area.base + "and a height of " + Area.height + "is " + Area.area());
+console.log(calcHypotenuse(base, height));
+var hypotenuse = calcHypotenuse(base, height);
+var message = "The hypotenuse of a triangle with a base of " + base + " and a height of " + height + " is " + hypotenuse + "."; 
+
+document.getElementById("two").innerHTML = message;
+
+// Part 3: Triangle object
+var triangle = {
+  base: 30,
+  height: 40,
+  canvasID: "canvas",
+  calcArea: function() {
+    return (1/2) * this.base * this.height;
+  },
+  calcHypotenuse: function() {
+    return Math.sqrt((this.base * this.base) + (this.height * this.height));
+  },
+  drawIt: function() {
+    drawTriangle(this.base, this.height, this.canvasId);
+  },
+};
+
+var message1 = "The area of a triangle with a base of " + base + " and a height of " + height + " is " + area + ".";
+var message2 = "The hypotenuse of a triangle with a base of " + base + " and a height of " + height + " is " + hypotenuse + ".";
+
+document.getElementById("three").innerHTML = message1 + message2;
+triangle.drawIt();
+
+
+ 
+
+
+
+
 
